c19282876c83f2deaf669e5284c2f9cb82e3a281
Yahyoxon <yahyoxonmuhammadaliyev567@gmail.com>
2022-09-10 21:12:38 +0500

    fixing commit

--- markazf/src/Pages/Qabul.jsx
+++ markazf/src/Pages/Qabul.jsx
@@ -1,131 +1,131 @@
 import { useEffect, useState } from "react";
 import axios from "../api/axios";
 import Input from "react-phone-number-input/input";
-
+import { useNavigate } from "react-router-dom";
 export default function Qabul() {
-  const [name, setName] = useState("");
   const [tel1, setTel1] = useState("");
   const [tel2, setTel2] = useState(null);
-  const [kurs, setKurs] = useState(1);
-  const [vaqt, setVaqt] = useState(0);
   const [izoh, setIzoh] = useState(null);
   const [kurslar, setKurslar] = useState([]);
   const [qabuls, setQabuls] = useState([]);
+  const [inputs, setInputs] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const getQabulFunc = async () => {
-      try {
-        const response = await axios.get("/qabul");
-        setQabuls(response.data);
-        // console.log(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getQabulFunc();
-    // const getKurslar = async () => {
-    //   try {
-    //     const response = await axios.get("http://127.0.0.1:8000/api/qabul");
-    //     // console.log(response.data);
-    //     setKurslar(res.data);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
-    getQabulFunc()
+    // getQabulFunc();
     // getKurslar()
   }, []);
 
-  const newQabul = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await axios.post(`/qabul`, {
-        name,
-        tel1,
-        tel2,
-        kurs,
-        vaqt,
-        izoh,
-        admin_id: 1,
-      });
-      console.log(res.data);
-    } catch (error) {
-      console.log(error);
-    }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs((values) => ({ ...values, [name]: value, tel1, tel2 }));
   };
+
+  const SubmitForm = () => {
+    axios.post("/qabul", inputs).then(() => {
+      navigate("/");
+    });
+  };
+
+  // const getQabulFunc = async () => {
+  //   try {
+  //     const response = await axios.get("/qabul");
+  //     setQabuls(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // const getKurslar = async () => {
+  //   try {
+  //     const response = await axios.get("http://127.0.0.1:8000/api/qabul");
+  //     // console.log(response.data);
+  //     setKurslar(res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // const newQabul = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await axios.post(`/qabul`, {
+  //       name,
+  //       tel1,
+  //       tel2,
+  //       kurs,
+  //       vaqt,
+  //       izoh,
+  //       admin_id: 1,
+  //     });
+  //     console.log(res.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <>
       <section className="con-md m-10 bg-slate-500 p-4 rounded-md " id="qabul">
-        <form onSubmit={newQabul} className="grid">
+        <div className="grid">
           <div className="m-4 grid-cols-6">
-            <label
-              for="text"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               Ism kiriting
             </label>
             <input
+              name="name"
               type="text"
-              onChange={(e) => setName(e.target.value)}
+              value={inputs.name || ""}
+              onChange={handleChange}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Ism yozing"
-              required=""
+              required
             />
           </div>
           <div className="m-4 grid-cols-6  ">
-            <label
-              for="tel1"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               1-telefon raqam
             </label>
             <div className="flex">
               <span className="mt-2 mr-2">+998</span>
               <Input
+                name="tel1"
                 international
-                countryCallingCodeEditable={false}
                 defaultCountry="UZ"
                 placeholder="1-telefon raqamni kiriting"
                 required
-                value={tel1}
                 onChange={setTel1}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               />
             </div>
           </div>
           <div className="m-4 grid-cols-6  ">
-            <label
-              for="tel2"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               2-telefon raqam
             </label>
             <div className="flex">
               <span className="mt-2 mr-2">+998</span>
               <Input
+                name="tel2"
                 international
-                countryCallingCodeEditable={false}
                 defaultCountry="UZ"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="2-telefon raqamni kiriting"
-                required=""
-                value={tel2}
+                required
                 onChange={setTel2}
               />
             </div>
           </div>
           <div className="flex justify-between">
             <div className="m-4 w-full ">
-              <label
-                for="kurs"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Kursni tanlang
               </label>
               <select
+                name="kurs"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                onChange={(e) => setKurs(e.target.value)}
+                onChange={handleChange}
               >
                 {/* <option value="1"> - - - </option> 
                  {kurslar.map((item) => (
@@ -137,14 +137,12 @@ export default function Qabul() {
               </select>
             </div>
             <div className="m-4 w-full ">
-              <label
-                for="kurs"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Kurs vaqti
               </label>
               <select
-                onChange={(e) => setVaqt(e.target.value)}
+                name="vaqt"
+                onChange={handleChange}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option value="0">- - -</option>
@@ -155,25 +153,24 @@ export default function Qabul() {
             </div>
           </div>
           <div className="m-4 grid-cols-6">
-            <label
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              htmlFor="textarea"
-            >
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
               Izoh
             </label>
             <textarea
+              name="izoh"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Izoh . . . .  . . . . . . . . "
-              onChange={(e) => setIzoh(e.target.value)}
+              onChange={handleChange}
             ></textarea>
           </div>
           <button
+            onClick={SubmitForm}
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Qo'shish
           </button>
-        </form>
+        </div>
       </section>
     </>
   );
